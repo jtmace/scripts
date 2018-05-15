b41133d08f324058db3fdf13884a4f3421244781
jtmace <23106317+jtmace@users.noreply.github.com>
2018-05-15 10:17:28 -0500

    Update block_RU.sh

--- block_RU.sh
+++ block_RU.sh
@@ -18,3 +18,22 @@ echo $R | sed -e 's/\s/\n/g' | sed 1,10d | while IFS="/" read IP SN; do
         M=$(( 0xffffffff ^ ((1 << (32-SN)) -1) ))
         echo "  network-object $IP $(( (M>>24) & 0xff )).$(( (M>>16) & 0xff )).$(( (M>>8) & 0xff )).$(( M & 0xff ))"
 done
+
+# Or using Python:
+#==============================================
+# #!/usr/bin/env python
+# import requests
+# 
+# r = requests.post("http://www.find-ip-address.org/ip-country/", data={'country': 'RU', 'output': 'CIDR', 'prefix': ''})
+# 
+# content = r.text[81:]
+# req = content.split('\n')
+# 
+# for x in req:
+#     (addr_str, cidr_str) = x.split('/')
+#     addr = addr_str.strip()
+#     cidr = int(cidr_str)
+#     mask = [0, 0, 0, 0]
+#     for i in range(cidr):
+#         mask[i/8] = mask[i/8] + (1 << (7 - i % 8))
+#         print 'network-object ' + addr + ' ' + ".".join(map(str, mask))
